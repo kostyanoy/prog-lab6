e99c7890ec5efab4b144234aeadb7c27a079b418
Kosty Manuhin <kostymanuhin@gmail.com>
2023-04-03 17:31:55 +0300

    gradle configuration

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,8 +1,13 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20-Beta"
+    id("org.jetbrains.dokka") version "1.7.20"
+    application
 }
 
-group = "org.example"
+group = "org.itmo"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,13 +15,43 @@ repositories {
 }
 
 dependencies {
+    val kotlinVersion = "1.8.0"
+    val koinVersion = "3.3.3"
+    val mockkVersion = "1.13.4"
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:${mockkVersion}")
+    testImplementation("io.insert-koin:koin-test-junit5:$koinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    implementation(kotlin("serialization", version = kotlinVersion))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("org.slf4j:slf4j-log4j12:2.0.6")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 }
+
 
 tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(8)
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "ServerKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+
+application {
+    mainClass.set("ServerKt")
 }
